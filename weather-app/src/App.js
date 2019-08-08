21382551b485dd96bd1dbb8fccb8bd7f69a19c55
Hoang Pham <tienhoang.pham@bc.fi>
2019-08-08 10:42:39 +0300

    add sample text and buttons to weather app

--- weather-app/src/App.js
+++ weather-app/src/App.js
@@ -1,26 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Titles from './components/titles';
+import Weather from './components/weather';
+import Form from './components/form';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return ( 
+      <div >
+        <Titles></Titles>
+        <Form></Form>
+        <Weather></Weather>
+      </div>
+    )
+  }
 }
-
 export default App;
