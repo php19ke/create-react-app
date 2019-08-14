8a3cfb13f4fc32711c9be4b574bbfde5f4b08e80
Hoang Pham <tienhoang.pham@bc.fi>
2019-08-14 12:10:09 +0300

    replace class component with functional component

--- hello-world/src/index.js
+++ hello-world/src/index.js
@@ -1,40 +1,5 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-
-const customStyle = {
-    'color': 'red',
-    'backgroundColor': 'yellow'
-};
-
-/* const helloMessage = React.createElement(
-    'div', // name of the html element to be created
-    {id: 'parentEl'}, // properties to be added, null if nothing
-    [React.createElement( // content of the element
-        'h2',
-        {id: 'childEl', style: customStyle},
-        'Hello World from Child El'
-    ),
-    React.createElement( // content of the element
-        'h2',
-        {id: 'childEl', style: customStyle},n
-        'Hello World from Child El'
-    ),
-    React.createElement( // content of the element
-        'h2',
-        {id: 'childEl', style: customStyle},
-        'Hello World from Child El'
-    )]
-); */
-
-/* Exercise: Create a React element using either JSX or without JSX method. The parent element should be a div and have a classname name-card, inside it there are 3 child elements, the first one is img with a random picture, second one is h4 with your name, third one is div with content of 64 likes */
-const imgSrc = 'https://upload.wikimedia.org/wikipedia/commons/thumb/7/7e/Circle-icons-profile.svg/240px-Circle-icons-profile.svg.png';
-
-/* const helloMessage = <div>
-    <img src={imgSrc}></img>
-    <h4>Hoang Pham</h4>
-    <div style={customStyle}>ON THE 11.9 WE HAVE VINCIT EXCURSION - 64 LIKES</div>
-    </div>
- */
+import React, { Component } from 'react';
+import { render } from 'react-dom';
 
 const greetingMessages = {
     morning: 'Good morning',
@@ -42,23 +7,57 @@ const greetingMessages = {
     evening: 'Good evening'
 }
 
-class HelloMessage extends React.Component {
-    render() {
-        console.log(this.props)
+const today = new Date();
+const hour = today.getHours();
+const min = today.getMinutes();
+const now = today.getHours() + ":" + (min < 10 ? '0' + min : min) + ":" + today.getSeconds();
 
+/* class HelloMessage extends Component {
+    
+    fetchGreetingMessage = time => {
+        if (time <= 12) {
+            return this.props.morningMessage;
+        } else if (time <= 18) {
+            return this.props.afternoonMessage;
+        } else {
+            return this.props.eveningMessage;
+        } 
+    }
+
+    render() {
         return (
             <div>
-                <h1>{this.props.morningMessage}</h1>   
+                <h1> { this.fetchGreetingMessage(this.props.timeNow) } </h1>   
+                <h1> the time now is { this.props.today } </h1>   
             </div>          
         )  
     }
+} */
+
+function fetchGreetingMessage (props, time) {
+    if (time <= 12) {
+        return props.morningMessage;
+    } else if (time <= 18) {
+        return props.afternoonMessage;
+    } else {
+        return props.eveningMessage;
+    } 
 }
 
-ReactDOM.render(
+function HelloMessage(props) {
+    return <div>
+        <h1> { fetchGreetingMessage(props, props.timeNow) } </h1>   
+        <h1> the time now is { props.today } </h1>   
+    </div>            
+}
+
+render(
     <HelloMessage 
         morningMessage={greetingMessages.morning}
         afternoonMessage={greetingMessages.afternoon}
         eveningMessage={greetingMessages.evening}
+        timeNow={hour}
+        today={now}
     />,
     document.getElementById('mainContent')
 );
