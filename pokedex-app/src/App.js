862ecbd24242ef128e3c7fdd422dd2557f912360
Hoang Pham <tienhoang.pham@bc.fi>
2019-08-22 10:22:39 +0300

    add pokemon dependency and render list of all pokemon

--- pokedex-app/src/App.js
+++ pokedex-app/src/App.js
@@ -2,18 +2,38 @@ import React, { Component } from 'react';
 import logo from './logo.png';
 import './App.css';
 
+const pokemon = require('pokemon');
+
 class App extends Component {
 	constructor() {
 		super();
 		this.state = {
-			pokemon: {}
+			loaded: false,
+			pokemons: []
 		};
+
+		this.fetchData = this.fetchData.bind(this);
+	}
+
+	fetchData() {
+		this.setState({
+			pokemons: pokemon.all(),
+			loaded: true
+		});
 	}
 
 	render() {
 		return (
 			<div className="App" >
 				<img src={logo} className="App-logo" alt="logo" />
+				<button onClick={this.fetchData}>Fetch</button>
+				{
+					this.state.loaded ?
+						this.state.pokemons.map((pokemon, index) => {
+							return <div key={index}>{pokemon}</div>
+						}) :
+						<div>Nothing loaded yet</div>
+				}
 			</div>
 		);
 	}
