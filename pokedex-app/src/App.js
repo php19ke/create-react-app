2985563dc6e576978d0591d987a2306345404bbe
Hoang Pham <tienhoang.pham@bc.fi>
2019-08-22 10:45:18 +0300

    add fetchDetails api call for each pokemon click

--- pokedex-app/src/App.js
+++ pokedex-app/src/App.js
@@ -10,17 +10,32 @@ class App extends Component {
 		super();
 		this.state = {
 			loaded: false,
-			pokemons: []
+			pokemons: [],
+			details: []
 		};
 
 		this.fetchData = this.fetchData.bind(this);
+		this.fetchDetails = this.fetchDetails.bind(this);
 	}
 
 	fetchData() {
 		this.setState({
-			pokemons: pokemon.all(),
+			pokemons: pokemon.all(), // return array of strings that contain pokemon names 
 			loaded: true
 		});
+	}
+
+	fetchDetails(e) {
+		// 'https://pokeapi.co/api/v2/pokemon/' + e.target.innerText.toLowerCase()
+		fetch(`https://pokeapi.co/api/v2/pokemon/${e.target.innerText.toLowerCase()}/`)
+			.then(data => data.json())
+			.then(data => {
+				alert(`
+					Here are the details information about ${data.name}:
+					${data.name} has ${data.abilities.length} abilities
+					And it is of type ${data.types[0].type.name} pokemon
+				`);
+			});
 	}
 
 	render() {
@@ -31,7 +46,7 @@ class App extends Component {
 				{
 					this.state.loaded ?
 						this.state.pokemons.map((pokemon, index) => {
-							return <div key={index}>{pokemon}</div>
+							return <div key={index} onClick={this.fetchDetails}>{pokemon}</div>
 						}) :
 						<div>Nothing loaded yet</div>
 				}
