5b451d2ba46209001efdf467ea868775a6affc6c
Hoang Pham <tienhoang.pham@bc.fi>
2019-09-10 10:04:00 +0300

    add eslint

--- bet-events/src/index.js
+++ bet-events/src/index.js
@@ -1,3 +1,5 @@
+// @flow
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 
@@ -12,9 +14,7 @@ class App extends React.Component {
         e.preventDefault();
         console.log(this.state.lang);
 
-        this.setState({
-            loading: true
-        });
+        this.setState({ loading: true });
 
         fetch('https://eu-offering.kambicdn.org/offering/api/v2/paf/event/live/open.json?lang=' + this.state.lang + '&market=FI')
             .then(data => data.json())
@@ -33,7 +33,7 @@ class App extends React.Component {
     }
 
     render() {
-        return(
+        return (
             <div>
                 <form onSubmit={this.startLoading}>
                     <label>Pick a random color
@@ -51,12 +51,12 @@ class App extends React.Component {
 
 
                 {
-                    this.state.loading ? 
-                    <div>LOADING CONTENT ...... </div> :
-                    (this.state.data.liveEvents ? // this line gets executed if this.state.loading === false
-                    this.state.data.liveEvents.map((event) => 
-                    <div>{event.event.name} {event.event.sport} {event.event.group}</div>) :
-                    console.log('Nope... its not there yet')) // this line gets executed if this.state.data.liveEvents is not defined
+                    this.state.loading ?
+                        <div>LOADING CONTENT ...... </div> :
+                        (this.state.data.liveEvents ? // this line gets executed if this.state.loading === false
+                            this.state.data.liveEvents.map((event) =>
+                                <div>{event.event.name} {event.event.sport} {event.event.group}</div>) :
+                            console.log('Nope... its not there yet')) // this line gets executed if this.state.data.liveEvents is not defined
                 }
             </div>
         )
